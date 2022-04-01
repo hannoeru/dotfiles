767b1c824cf24ef7d34fcdaa51fda07e1f4fe2ee
hanlee <me@hanlee.co>
2022-04-01 16:31:36 +0900

    feat: use exa to replace ls command

--- private_dot_config/fish/config-osx.fish
+++ private_dot_config/fish/config-osx.fish
@@ -6,3 +6,10 @@ alias dl="cd ~/Downloads"
 alias p="cd ~/Project"
 alias wine="wine64"
 alias brewup="brew update; brew upgrade; brew prune; brew cleanup"
+
+if type -q exa
+  alias l="exa -g --icons"
+  alias ls="l"
+  alias ll="l -l"
+  alias lla="ll -a"
+end
