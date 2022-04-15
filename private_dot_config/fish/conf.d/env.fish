2bf4629dce83c95bbe73c7f5bbef60fab6571ed0
hanlee <me@hanlee.co>
2022-04-15 21:56:41 +0900

    fix: use fish_add_path

--- private_dot_config/fish/conf.d/env.fish
+++ private_dot_config/fish/conf.d/env.fish
@@ -1,52 +1,52 @@
 # bin folder
-add_to_path "$HOME/bin"
+fish_add_path "$HOME/bin"
 # Custom scripts
-add_to_path "$HOME/.local/bin"
+fish_add_path "$HOME/.local/bin"
 # OpenSSL
-add_to_path "$HOMEBREW_PREFIX/opt/openssl/bin"
+fish_add_path "$HOMEBREW_PREFIX/opt/openssl/bin"
 
 #
 # Node
 #
 
 # npm
-add_to_path "$HOME/.npm-global/bin"
+fish_add_path "$HOME/.npm-global/bin"
 
 # pnpm
 set -gx PNPM_HOME "$HOME/Library/pnpm"
-add_to_path "$PNPM_HOME"
+fish_add_path "$PNPM_HOME"
 
 #
 # Python
 #
 
 set -gx PYENV_ROOT "$HOME/.pyenv"
-add_to_path "$PYENV_ROOT/shims"
+fish_add_path "$PYENV_ROOT/shims"
 
 #
 # Deno
 #
 
 set -gx DENO_DIR "$HOME/.deno"
-add_to_path "$HOME/.deno/bin"
+fish_add_path "$HOME/.deno/bin"
 
 #
 # Golang
 #
 
 set -gx GOPATH "$HOME/go"
-add_to_path "$GOPATH/bin"
+fish_add_path "$GOPATH/bin"
 
 #
 # Rust
 #
 
 set -gx CARGOPATH "$HOME/.cargo"
-add_to_path "$CARGOPATH/bin"
+fish_add_path "$CARGOPATH/bin"
 
 #
 # Android
 #
 
 set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
-add_to_path "$ANDROID_HOME/bin"
+fish_add_path "$ANDROID_HOME/bin"
--- private_dot_config/fish/functions/add_to_path.fish
+++
@@ -1,3 +0,0 @@
-function add_to_path
-  set -U fish_user_paths $argv[1] $fish_user_paths
-end
