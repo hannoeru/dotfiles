2bded271e8ebfe55282b4727385c8f449874c48d
hanlee <me@hanlee.co>
2022-12-13 14:45:04 +0900

    chore: remove miniconda init

--- private_dot_config/zsh/conf.d/miniconda.zsh
+++
@@ -1,4 +0,0 @@
-# init miniconda
-if [ -e $HOME/.pyenv/versions/miniconda3-latest/bin/conda ]; then
-  eval "$('$HOME/.pyenv/versions/miniconda3-latest/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-fi
